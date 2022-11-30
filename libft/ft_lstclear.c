8c516ae5e64e99a5ebf39b3274d72d40f1f0b076
WizeAnzek <105432454+WizeAnzek@users.noreply.github.com>
2022-11-30 13:43:20 +0100

    Add files via upload

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	if (!del)
+		return ;
+	while (*lst)
+	{
+		del((*lst)->content);
+		tmp = *lst;
+		*lst = tmp->next;
+		free(tmp);
+	}
+	*lst = NULL;
+}
