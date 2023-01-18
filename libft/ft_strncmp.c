dbfc4fb955e706ba8a06944841393b818eba7db9
WizeAnzek <105432454+WizeAnzek@users.noreply.github.com>
2023-01-18 12:33:11 +0100

    Update ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -7,7 +7,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i  = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] == s2[i] && i < n && s1[i] && s2[i])
+	while ((unsigned char)s1[i] == (unsigned char)s2[i] && i < n && s1[i] && s2[i])
 		i++;
 	if (i == n)
 		i--;
